f2bb3909fe6e7e817f6dc732c733048701d2c4e2
Camilo Diaz <camilo.diaz@2brains.cl>
2019-04-10 17:12:30 -0400

    firt commit

---
+++ src/components/WeatherLocation/WeatherData/index.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import WeatherExtraInfo from './WeatherExtraInfo';
+import WeatherTemperature from './WeatherTemperature';
+import {
+    CLOUD,
+    CLOUDY,
+    SUN,
+    RAIN,
+    SNOW,
+    WINDY,
+} from '../../../constants/weathers';
+import './styles.css';
+
+const WeatherData = () => (
+    <div className="weatherDataCont" >
+        <WeatherTemperature
+            temperature={20}
+            weatherState={SNOW} />
+        <WeatherExtraInfo humidity={80} wind={"10 m/s"} />
+    </div>
+
+);
+export default WeatherData;
